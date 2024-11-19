4998ae2e8b212b945ed4e07280908c1def821edf
김지훈 <kim@jihun.io>
2024-11-19 14:57:23 +0900

    방송사 로고 상수 수정

--- src/constants/stations.ts
+++ src/constants/stations.ts
@@ -1,75 +1,73 @@
-import bbsLogo from '../public/images/stations/bbs.svg';
-import cbsLogo from '../public/images/stations/cbs.svg';
-import cpbcLogo from '../public/images/stations/cpbc.svg';
-import ebsLogo from '../public/images/stations/ebsfm.svg';
-import febcLogo from '../public/images/stations/febc.svg';
-import kbs1Logo from '../public/images/stations/kbs1.svg';
-import kbs2Logo from '../public/images/stations/kbs2.svg';
-import kbs3Logo from '../public/images/stations/kbs3.svg';
-import kbs1fmLogo from '../public/images/stations/kbs1fm.svg';
-import kbs2fmLogo from '../public/images/stations/kbs2fm.svg';
-import kbshanminjokLogo from '../public/images/stations/kbshanminjok.svg';
-import kookbangLogo from '../public/images/stations/kookbang.svg';
-import kugakLogo from '../public/images/stations/kugak.svg';
-import mbcfm4uLogo from '../public/images/stations/mbcfm4u.svg';
-import mbcsfmLogo from '../public/images/stations/mbcsfm.svg';
-import sbslovefmLogo from '../public/images/stations/sbslovefm.svg';
-import sbspowerfmLogo from '../public/images/stations/sbspowerfm.svg';
-import tbsefmLogo from '../public/images/stations/tbsefm.svg';
-import tbsfmLogo from '../public/images/stations/tbsfm.svg';
-import ytnLogo from '../public/images/stations/ytn.svg';
-import tbnLogo from '../public/images/stations/tbn.svg';
-import obsLogo from '../public/images/stations/obs.svg';
-import ifmLogo from '../public/images/stations/ifm.svg';
-import afnLogo from '../public/images/stations/afn.svg';
-import wbsLogo from '../public/images/stations/wbs.svg';
-
-
+const bbsLogo: string = "./images/stations/bbs.svg";
+const cbsLogo: string = "./images/stations/cbs.svg";
+const cpbcLogo: string = "./images/stations/cpbc.svg";
+const ebsLogo: string = "./images/stations/ebsfm.svg";
+const febcLogo: string = "./images/stations/febc.svg";
+const kbs1Logo: string = "./images/stations/kbs1.svg";
+const kbs2Logo: string = "./images/stations/kbs2.svg";
+const kbs3Logo: string = "./images/stations/kbs3.svg";
+const kbs1fmLogo: string = "./images/stations/kbs1fm.svg";
+const kbs2fmLogo: string = "./images/stations/kbs2fm.svg";
+const kbshanminjokLogo: string = "./images/stations/kbshanminjok.svg";
+const kookbangLogo: string = "./images/stations/kookbang.svg";
+const kugakLogo: string = "./images/stations/kugak.svg";
+const mbcfm4uLogo: string = "./images/stations/mbcfm4u.svg";
+const mbcsfmLogo: string = "./images/stations/mbcsfm.svg";
+const sbslovefmLogo: string = "./images/stations/sbslovefm.svg";
+const sbspowerfmLogo: string = "./images/stations/sbspowerfm.svg";
+const tbsefmLogo: string = "./images/stations/tbsefm.svg";
+const tbsfmLogo: string = "./images/stations/tbsfm.svg";
+const ytnLogo: string = "./images/stations/ytn.svg";
+const tbnLogo: string = "./images/stations/tbn.svg";
+const obsLogo: string = "./images/stations/obs.svg";
+const ifmLogo: string = "./images/stations/ifm.svg";
+const afnLogo: string = "./images/stations/afn.svg";
+const wbsLogo: string = "./images/stations/wbs.svg";
 
 export interface RadioStation {
   id: string;
   name: string;
   streamUrl: string;
   color?: string;
-  logo: typeof import("*.svg").default;
+  logo: string;
 }
 
 // KBS 방송국
 export const KBS_STATIONS: RadioStation[] = [
   {
-    id: 'kbs1',
-    name: 'KBS 1라디오',
-    streamUrl: '?stn=kbs&ch=1radio',
+    id: "kbs1",
+    name: "KBS 1라디오",
+    streamUrl: "?stn=kbs&ch=1radio",
     logo: kbs1Logo,
   },
   {
-    id: 'kbs2',
-    name: 'KBS 2라디오',
-    streamUrl: '?stn=kbs&ch=2radio',
+    id: "kbs2",
+    name: "KBS 2라디오",
+    streamUrl: "?stn=kbs&ch=2radio",
     logo: kbs2Logo,
   },
   {
-    id: 'kbs3',
-    name: 'KBS 3라디오',
-    streamUrl: '?stn=kbs&ch=3radio',
+    id: "kbs3",
+    name: "KBS 3라디오",
+    streamUrl: "?stn=kbs&ch=3radio",
     logo: kbs3Logo,
   },
   {
-    id: 'kbs1fm',
-    name: 'KBS 1FM',
-    streamUrl: '?stn=kbs&ch=1fm',
+    id: "kbs1fm",
+    name: "KBS 1FM",
+    streamUrl: "?stn=kbs&ch=1fm",
     logo: kbs1fmLogo,
   },
   {
-    id: 'kbs2fm',
-    name: 'KBS 2FM',
-    streamUrl: '?stn=kbs&ch=2fm',
+    id: "kbs2fm",
+    name: "KBS 2FM",
+    streamUrl: "?stn=kbs&ch=2fm",
     logo: kbs2fmLogo,
   },
   {
-    id: 'kbshanminjok',
-    name: 'KBS 한민족방송',
-    streamUrl: '?stn=kbs&ch=hanminjok',
+    id: "kbshanminjok",
+    name: "KBS 한민족방송",
+    streamUrl: "?stn=kbs&ch=hanminjok",
     logo: kbshanminjokLogo,
   },
 ];
@@ -77,15 +75,15 @@ export const KBS_STATIONS: RadioStation[] = [
 // MBC 방송국
 export const MBC_STATIONS: RadioStation[] = [
   {
-    id: 'mbcsfm',
-    name: 'MBC 표준FM',
-    streamUrl: '?stn=mbc&ch=sfm',
+    id: "mbcsfm",
+    name: "MBC 표준FM",
+    streamUrl: "?stn=mbc&ch=sfm",
     logo: mbcsfmLogo,
   },
   {
-    id: 'mbcfm4u',
-    name: 'MBC FM4U',
-    streamUrl: '?stn=mbc&ch=fm4u',
+    id: "mbcfm4u",
+    name: "MBC FM4U",
+    streamUrl: "?stn=mbc&ch=fm4u",
     logo: mbcfm4uLogo,
   },
 ];
@@ -93,15 +91,15 @@ export const MBC_STATIONS: RadioStation[] = [
 // SBS 방송국
 export const SBS_STATIONS: RadioStation[] = [
   {
-    id: 'sbslovefm',
-    name: 'SBS 러브FM',
-    streamUrl: '?stn=sbs&ch=lovefm',
+    id: "sbslovefm",
+    name: "SBS 러브FM",
+    streamUrl: "?stn=sbs&ch=lovefm",
     logo: sbslovefmLogo,
   },
   {
-    id: 'sbspowerfm',
-    name: 'SBS 파워FM',
-    streamUrl: '?stn=sbs&ch=powerfm',
+    id: "sbspowerfm",
+    name: "SBS 파워FM",
+    streamUrl: "?stn=sbs&ch=powerfm",
     logo: sbspowerfmLogo,
   },
 ];
@@ -109,9 +107,9 @@ export const SBS_STATIONS: RadioStation[] = [
 // EBS
 export const EBS_STATIONS: RadioStation[] = [
   {
-    id: 'ebsfm',
-    name: 'EBS FM',
-    streamUrl: '?stn=ebs',
+    id: "ebsfm",
+    name: "EBS FM",
+    streamUrl: "?stn=ebs",
     logo: ebsLogo,
   },
 ];
@@ -119,15 +117,15 @@ export const EBS_STATIONS: RadioStation[] = [
 // 지역 방송
 export const LOCAL_STATIONS: RadioStation[] = [
   {
-    id: 'obs',
-    name: 'OBS 라디오',
-    streamUrl: '?stn=obs',
+    id: "obs",
+    name: "OBS 라디오",
+    streamUrl: "?stn=obs",
     logo: obsLogo,
   },
   {
-    id: 'ifm',
-    name: 'iFM 경인방송',
-    streamUrl: '?stn=ifm',
+    id: "ifm",
+    name: "iFM 경인방송",
+    streamUrl: "?stn=ifm",
     logo: ifmLogo,
   },
 ];
@@ -135,9 +133,9 @@ export const LOCAL_STATIONS: RadioStation[] = [
 // YTN
 export const YTN_STATIONS: RadioStation[] = [
   {
-    id: 'ytn',
-    name: 'YTN 라디오',
-    streamUrl: '?stn=ytn',
+    id: "ytn",
+    name: "YTN 라디오",
+    streamUrl: "?stn=ytn",
     logo: ytnLogo,
   },
 ];
@@ -145,15 +143,15 @@ export const YTN_STATIONS: RadioStation[] = [
 // TBS
 export const TBS_STATIONS: RadioStation[] = [
   {
-    id: 'tbsfm',
-    name: 'TBS FM',
-    streamUrl: '?stn=tbs&ch=fm',
+    id: "tbsfm",
+    name: "TBS FM",
+    streamUrl: "?stn=tbs&ch=fm",
     logo: tbsfmLogo,
   },
   {
-    id: 'tbsefm',
-    name: 'TBS eFM',
-    streamUrl: '?stn=tbs&ch=efm',
+    id: "tbsefm",
+    name: "TBS eFM",
+    streamUrl: "?stn=tbs&ch=efm",
     logo: tbsefmLogo,
   },
 ];
@@ -161,9 +159,9 @@ export const TBS_STATIONS: RadioStation[] = [
 // TBN
 export const TBN_STATIONS: RadioStation[] = [
   {
-    id: 'tbn',
-    name: 'TBN 경인교통방송',
-    streamUrl: '?stn=tbn',
+    id: "tbn",
+    name: "TBN 경인교통방송",
+    streamUrl: "?stn=tbn",
     logo: tbnLogo,
   },
 ];
@@ -171,21 +169,21 @@ export const TBN_STATIONS: RadioStation[] = [
 // CBS
 export const CBS_STATIONS: RadioStation[] = [
   {
-    id: 'cbssfm',
-    name: 'CBS 표준FM',
-    streamUrl: '?stn=cbs&ch=sfm',
+    id: "cbssfm",
+    name: "CBS 표준FM",
+    streamUrl: "?stn=cbs&ch=sfm",
     logo: cbsLogo,
   },
   {
-    id: 'cbsmfm',
-    name: 'CBS 음악FM',
-    streamUrl: '?stn=cbs&ch=mfm',
+    id: "cbsmfm",
+    name: "CBS 음악FM",
+    streamUrl: "?stn=cbs&ch=mfm",
     logo: cbsLogo,
   },
   {
-    id: 'cbsjoy4u',
-    name: 'CBS JOY4U',
-    streamUrl: '?stn=cbs&ch=joy4u',
+    id: "cbsjoy4u",
+    name: "CBS JOY4U",
+    streamUrl: "?stn=cbs&ch=joy4u",
     logo: cbsLogo,
   },
 ];
@@ -193,27 +191,27 @@ export const CBS_STATIONS: RadioStation[] = [
 // 종교방송
 export const RELIGIOUS_STATIONS: RadioStation[] = [
   {
-    id: 'febc',
-    name: 'FEBC 서울극동방송',
-    streamUrl: '?stn=febc',
+    id: "febc",
+    name: "FEBC 서울극동방송",
+    streamUrl: "?stn=febc",
     logo: febcLogo,
   },
   {
-    id: 'bbs',
-    name: 'BBS 서울불교방송',
-    streamUrl: '?stn=bbs',
+    id: "bbs",
+    name: "BBS 서울불교방송",
+    streamUrl: "?stn=bbs",
     logo: bbsLogo,
   },
   {
-    id: 'cpbc',
-    name: 'CPBC 가톨릭평화방송',
-    streamUrl: '?stn=cpbc',
+    id: "cpbc",
+    name: "CPBC 가톨릭평화방송",
+    streamUrl: "?stn=cpbc",
     logo: cpbcLogo,
   },
   {
-    id: 'wbs',
-    name: 'WBS 서울원음방송',
-    streamUrl: '?stn=wbs',
+    id: "wbs",
+    name: "WBS 서울원음방송",
+    streamUrl: "?stn=wbs",
     logo: wbsLogo,
   },
 ];
@@ -221,23 +219,35 @@ export const RELIGIOUS_STATIONS: RadioStation[] = [
 // 특수방송
 export const SPECIAL_STATIONS: RadioStation[] = [
   {
-    id: 'kookbang',
-    name: '국방FM',
-    streamUrl: '?stn=kookbang',
+    id: "kookbang",
+    name: "국방FM",
+    streamUrl: "?stn=kookbang",
     logo: kookbangLogo,
   },
   {
-    id: 'kugak',
-    name: '국악방송',
-    streamUrl: '?stn=kugak',
+    id: "kugak",
+    name: "국악방송",
+    streamUrl: "?stn=kugak",
     logo: kugakLogo,
   },
   {
-    id: 'afn',
-    name: 'AFN FM Humphreys',
-    streamUrl: '?stn=afn&city=humphreys',
+    id: "afn",
+    name: "AFN FM Humphreys",
+    streamUrl: "?stn=afn&city=humphreys",
     logo: afnLogo,
   },
 ];
 
-export const ALL_STATIONS = [...KBS_STATIONS, ...MBC_STATIONS, ...SBS_STATIONS, ...EBS_STATIONS, ...LOCAL_STATIONS, ...YTN_STATIONS, ...TBS_STATIONS, ...TBN_STATIONS, ...CBS_STATIONS, ...RELIGIOUS_STATIONS, ...SPECIAL_STATIONS];  
+export const ALL_STATIONS = [
+  ...KBS_STATIONS,
+  ...MBC_STATIONS,
+  ...SBS_STATIONS,
+  ...EBS_STATIONS,
+  ...LOCAL_STATIONS,
+  ...YTN_STATIONS,
+  ...TBS_STATIONS,
+  ...TBN_STATIONS,
+  ...CBS_STATIONS,
+  ...RELIGIOUS_STATIONS,
+  ...SPECIAL_STATIONS,
+];
