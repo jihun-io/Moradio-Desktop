ea99f1349a9fe191b0a67d6b8da653641a2caf49
김지훈 <kim@jihun.io>
2024-11-21 15:05:02 +0900

    환경 변수의 타입 오류 수정

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -1,16 +1,21 @@
 export interface IElectronAPI {
   env: {
     API_URL: string;
-    // 다른 환경변수들도 여기에 추가
     [key: string]: string | undefined;
   };
   send: (channel: string, data: any) => void;
   receive: (channel: string, func: Function) => void;
-  // 다른 preload에서 노출한 API들도 여기에 추가
 }
 
 declare global {
   interface Window {
-    electron: IElectronAPI;
+    electron: {
+      env: {
+        API_URL: string;
+        [key: string]: any;
+      };
+    };
   }
 }
+
+export {};
